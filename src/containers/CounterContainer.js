b44ef36fba2bb7448cb87a1c1a4de66d9f1a8924
우엥 <105274015+rnldudns@users.noreply.github.com>
2023-05-18 20:12:24 +0900

    fix: -> bindActionCreators 유틸함수

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,7 +1,22 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import Counter from "../components/Counter";
+import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = () => {
-    return <Counter />;
+const CounterContainer = ({ number, increase, decrease }) => {
+    return (
+        <Counter number={ number } onIncrease={ increase } onDecrease={ decrease }/>
+    );
 };
 
-export default CounterContainer;
+
+
+export default connect(
+    state => ({
+        number: state.counter.number,
+    }),
+    {
+        increase,
+        decrease,
+    },
+)(CounterContainer);
