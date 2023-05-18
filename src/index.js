4c31b5df54950a931c78e8c6b2b88da04f0f26f6
우엥 <105274015+rnldudns@users.noreply.github.com>
2023-05-18 19:51:54 +0900

    create counterContainer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from "./App";
 import { createStore } from "redux";
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools, devToolsEnhancer } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,devToolsEnhancer());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
