cb0709d5a31cb01298600a0bf69cf7dabda94880
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-06-14 23:28:59 +0900

    useSelector로 상태조회하기

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,22 +1,9 @@
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Counter from "../components/Counter";
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-    return (
-        <Counter number={ number } onIncrease={ increase } onDecrease={ decrease }/>
-    );
-};
-
-
-
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    {  //두번째 파라미터를 객체형태로 넣어주면 connect함수가 내부적으로 bindActionCreators 작업을 대신해 준다..
-        increase,
-        decrease,
-    },
-)(CounterContainer);
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    return <Counter number={number} />;
+}
+export default CounterContainer;
