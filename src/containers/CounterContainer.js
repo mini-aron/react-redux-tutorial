64d5ec49561ad94b39760d94432e8240210a4c86
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-06-14 23:38:33 +0900

    Update CounterContainer.js

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,9 +1,14 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from "../components/Counter";
 import { increase, decrease } from '../modules/counter';
 
 const CounterContainer = () => {
-    const number = useSelector(state => state.counter.number);
-    return <Counter number={number} />;
+    const number = useSelector(state => state.counter.number); // connect 함수를 사용하지 않고도 리덕스의 한태를 조회할 수 있게 해주는 훅이다..
+    const dispatch = useDispatch();
+    return (<Counter
+         number={number}
+         onIncrease={() => dispatch(increase())}
+         onDecrease={() => dispatch(decrease())}
+         />);
 }
 export default CounterContainer;
