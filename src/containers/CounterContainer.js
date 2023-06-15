1661cf5a6dac074820bea277674394259fa59cc4
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-06-15 20:30:06 +0900

    성능 최적화

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,14 +1,19 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter";
-import { increase, decrease } from '../modules/counter';
+import { increase, decrease } from "../modules/counter";
 
 const CounterContainer = () => {
-    const number = useSelector(state => state.counter.number); // connect 함수를 사용하지 않고도 리덕스의 한태를 조회할 수 있게 해주는 훅이다..
-    const dispatch = useDispatch();
-    return (<Counter
-         number={number}
-         onIncrease={() => dispatch(increase())}
-         onDecrease={() => dispatch(decrease())}
-         />);
-}
+  const number = useSelector((state) => state.counter.number); // connect 함수를 사용하지 않고도 리덕스의 한태를 조회할 수 있게 해주는 훅이다..
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch])
+  return (
+    <Counter
+      number={number}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+    />
+  );
+};
 export default CounterContainer;
